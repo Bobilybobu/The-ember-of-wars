2fc44b945621ee848310c729942ebf800b1c5aec
Noah <quanhbui2007@gmail.com>
2025-04-25 17:26:01 +0700

    updtate 25/4

--- app/layout.js
+++ app/layout.js
@@ -2,9 +2,9 @@ export default function Layout({children}) {
     return (
         <html> 
             <head>
-
+                <title> The Embers of War</title>
             </head>
-            <body>
+            <body className="w-screen h-screen bg-black">
                 {children}
             </body>
 
--- app/page.js
+++ app/page.js
@@ -1,7 +1,12 @@
 export default function Main(){
     return (
-        <div>
-            Hello
+        <div className = "bg-green">
+            <div className = "flex flex-row"> 
+                <div>intro</div>
+                <div>events</div>
+                <div>agent orange insight</div>
+                <div>join</div>
+            </div>
         </div>
     )
 }
